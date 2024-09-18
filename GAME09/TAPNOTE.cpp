04729640e8ac5c4b878142796d299fe017aff09e
晴輝 松山 <matu4034@gmail.com>
2024-09-18 14:48:53 +0900

    add TAPNOTE class

---
+++ GAME09/TAPNOTE.cpp
@@ -0,0 +1,21 @@
+#include "TAPNOTE.h"
+
+namespace GAME09 {
+	TAPNOTE::TAPNOTE(class GAME* game) :
+		NOTE(game) {
+	}
+	TAPNOTE::~TAPNOTE() {
+	}
+
+	void TAPNOTE::init() {
+		NOTE::init();
+	}
+
+	void TAPNOTE::update() {
+		NOTE::update();
+	}
+
+	void TAPNOTE::draw() {
+		NOTE::draw();
+	}
+}
