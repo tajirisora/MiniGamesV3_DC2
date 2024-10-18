bcc94cc7916da206cb0fefb61d7bd63960e418f5
晴輝 松山 <matu4034@gmail.com>
2024-10-18 11:31:36 +0900

    add BUTTON class

---
+++ GAME09/BUTTON.cpp
@@ -0,0 +1,78 @@
+#include "../../libOne/inc/graphic.h"
+#include "../../libOne/inc/input.h"
+#include "../../libOne/inc/mathUtil.h"
+#include "CONTAINER.h"
+#include "GAME09.h"
+#include "BUTTON.h"
+
+namespace GAME09 {
+    BUTTON::BUTTON(class GAME* game) :
+        GAME_OBJECT(game) {
+
+    }
+    BUTTON::~BUTTON() {
+
+    }
+    void BUTTON::create() {
+
+    }
+    void BUTTON::init() {
+        
+    }
+    void BUTTON::update() {
+        IsClick = false;
+        if (collision()) {
+            if (isTrigger(MOUSE_LBUTTON)) {
+                IsClick = true;
+            }
+        }
+    }
+    void BUTTON::draw() {
+        rectMode(CENTER);
+        image(Button.img, Button.pos.x, Button.pos.y, 0, Button.imgSize);
+        if (Button.debugFlag) {
+            fill(255, 0, 0, 100);
+            noStroke();
+            if (Button.colliType == CIRCLE) {
+                circle(Button.pos.x + Button.ofst.x, Button.pos.y + Button.ofst.y, Button.radius * 2);
+            }
+            else if (Button.colliType == RECT) {
+                rect(Button.pos.x + Button.ofst.x, Button.pos.y + Button.ofst.y,
+                    Button.size.x, Button.size.y);
+            }
+            else {
+                float len = Button.radius / Sqrt(2.0f) * 2;
+                rect(Button.pos.x + Button.ofst.x, Button.pos.y + Button.ofst.y,
+                    len, len);
+            }
+        }
+    }
+    bool BUTTON::collision() {
+        VECTOR2 mousePos(mouseX, mouseY);
+        VECTOR2 toMouse = mousePos - (Button.pos + Button.ofst);
+        if (Button.colliType == CIRCLE) {
+            if (toMouse.magSq() < Button.radius * Button.radius) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+        else if (Button.colliType == RECT) {
+            if (Abs(toMouse.x) < Button.size.x / 2.0f && Abs(toMouse.y) < Button.size.y / 2.0f) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+        else if (Button.colliType == DIAMOND) {
+            if (Abs(toMouse.x) + Abs(toMouse.y) < Button.radius) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+    }
+}
---
+++ GAME09/BUTTON.h
@@ -0,0 +1,40 @@
+#pragma once
+#include "GAME_OBJECT.h"
+#include "../../libOne/inc/VECTOR2.h"
+
+namespace GAME09 {
+    class BUTTON :
+        public GAME_OBJECT
+    {
+    public:
+        enum ColliType {
+            CIRCLE,
+            RECT,
+            DIAMOND
+        };
+
+        struct DATA {
+            int img;
+            float imgSize;
+            VECTOR2 pos;
+            ColliType colliType;
+            VECTOR2 ofst;
+            float radius;
+            VECTOR2 size;
+            bool debugFlag = true;
+        };
+    protected:
+        DATA Button;
+        bool IsClick;
+    public:
+        BUTTON(class GAME* game);
+        ~BUTTON();
+        virtual void create();
+        virtual void init();
+        virtual void update();
+        virtual void draw();
+        virtual bool isClick() { return IsClick; }
+    protected:
+        virtual bool collision();
+    };
+}
