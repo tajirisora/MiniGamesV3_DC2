3c952222aeb2e12ed317c78a9205e86de5729ba0
kenn4DC <kenn@mtc.biglobe.ne.jp>
2025-01-08 18:33:30 +0900

    text output

--- GAME15/GAME15.h
+++ GAME15/GAME15.h
@@ -11,5 +11,10 @@ namespace GAME15
         int create();
         void proc();
         void destroy();
+        void play();
+        void move();
+        void draw();
+    private:
+        class PLAYER* Player{};
     };
 }
