bcbf6479e348a773e66fd0d81ac00ba6c03abaf8
shu.1222.ya@icloud.com <shu.1222.ya@icloud.com>
2025-01-10 11:25:43 +0900

    game.cpp

--- GAME00/GAME00.h
+++ GAME00/GAME00.h
@@ -1,5 +1,7 @@
 #pragma once
 #include "../MAIN/GAME_BASE.h"
+#include<vector>
+#include<sstream>
 namespace GAME00
 {
     class GAME :
@@ -9,7 +11,37 @@ namespace GAME00
         GAME(class MAIN* main) :GAME_BASE(main) {};
         ~GAME() {};
         int create();
-        void proc();
         void destroy();
+        void Titel(); 
+        void proc();
+        void Init();
+        void Play();
+        void Play2();
+        void Play3();
+        void Draw();
+        void Draw2();
+        void Draw3();
+        void EnemyDraw();
+        void EnemyDraw2();
+        void Clear();
+
+        int card_backImg = 0;
+
+        float card1 = 0;
+        float card2 = 0;
+        float card3 = 0;
+        float card4 = 0;
+
+        float eCard1 = 0;
+        float eCard2 = 0;
+        float eCard3 = 0;
+        float eCard4 = 0;
+        
+        float score = 0;
+        float enemyScore = 0;
+      
+    private:
+        enum STATE{ TITLE,PLAY,PLAY2,PLAY3,CLEAR};
+        STATE State = TITLE;
     };
 }
