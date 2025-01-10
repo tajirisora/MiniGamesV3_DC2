86020ebdc34f1d74838a020da74a6a4e666d91cc
桃香 福田 <momoka_moka@icloud.com>
2025-01-10 12:54:40 +0900

    途中経過

--- GAME08/GAME08.h
+++ GAME08/GAME08.h
@@ -11,5 +11,24 @@ namespace GAME08
         int create();
         void proc();
         void destroy();
+        void title();
+        void init();
+        void play();
+        void result();
+        void draw();
+
+    private:
+        enum {
+            TITLE,
+            PLAY,
+            RESULT
+        };
+
+        int BackImg, TitleImg;
+        int PlayGameImg[15];
+
+
+        int STATE = TITLE;
+
     };
 }
